5001017cf65cb80ddfa55637dedb943270ccdf7a
Filip Budiša <filip@circuitmess.com>
2023-10-27 15:36:00 +0200

    Fix decodeDriveDir

--- src/CommData.cpp
+++ src/CommData.cpp
@@ -3,8 +3,6 @@
 #include <cmath>
 
 uint8_t CommData::encodeDriveDir(DriveDir dir){
-	//highest 4-bit value indicating direction
-	//Lowest 4 bits indicate speed in that direction
 	dir.dir = std::clamp(dir.dir, (uint8_t)0, (uint8_t)7);
 	dir.speed = std::clamp(dir.speed, 0.0f, 1.0f);
 
@@ -17,7 +15,7 @@ DriveDir CommData::decodeDriveDir(uint8_t raw){
 	uint8_t speed = raw >> 3;
 	uint8_t dir = raw & 0b111;
 
-	return { dir, (float) speed };
+	return { dir, (float) speed / 31.0f };
 }
 
 uint8_t CommData::encodeModulePlug(ModulePlugData plugData){
